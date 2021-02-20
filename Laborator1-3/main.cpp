9c01ca9f4caadc2208defed42b41352543132865
MicuRazvanFlorentin <florentin.razvan@yahoo.com>
2021-02-20 21:59:22 +0200

    Update main.cpp

--- Laborator1-3/main.cpp
+++ Laborator1-3/main.cpp
@@ -11,14 +11,13 @@ int main()
 {
     cuvinte V[300], aux;
     int nr = 0, i, j;
-    FILE *f = fopen("text.in", " r ");
     char x[1000];
-    fscanf( f, "%[^\n]s", x );
+    scanf( "%[^\n]s", x );
     char* p = strtok(x, " ");
     while (p != NULL)
     {
         nr++;
-        strcpy(V[nr].s, p);
+        strcpy( V[nr].s , p);
         V[nr].lungime = strlen( V[nr].s );
         p = strtok(NULL, " ");
     }
@@ -31,6 +30,14 @@ int main()
                 V[i]=V[j];
                 V[j]=aux;
             }
+            else
+                if( V[i].lungime == V[j].lungime )
+                    if( strcmp( V[i].s , V[j].s ) > 0 )
+                    {
+                        aux=V[i];
+                        V[i]=V[j];
+                        V[j]=aux;
+                    }
     for ( i = 1 ; i <= nr ; i++ )
     {
         printf( "%s\n" , V[i].s );
